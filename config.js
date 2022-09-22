ddc7d7db9d323c52c6036f199f43558adf053152
Smokey <114130006+Smokeyop20@users.noreply.github.com>
2022-09-22 22:18:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ global.mess = {
     grouponly: 'This command is only made for *Groups*, Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
     botonly: 'Only the *Bot itself* can use this command!',
-    waiting: 'Chotto Matte...',
+    waiting: 'Ruko jara sabar karo...',
     nolink: 'Please provide me *link*, Baka!',
     error: 'An error occurd!',
     banned: 'You are *Banned* fron using commands!',
